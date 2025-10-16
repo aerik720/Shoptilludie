0f52890212572a08a03b5e6fb7bf4532ebab503a
Lilu95Lilu <195738051+Lilu95Lilu@users.noreply.github.com>
2025-10-16 16:13:55 +0200

    First commit

---
+++ control.js
@@ -0,0 +1,3 @@
+export class control {
+    constructor() {}
+}
---
+++ model.js
@@ -0,0 +1,15 @@
+export class model {
+    constructor() { }
+
+    async fetchProducts() {
+        try {
+            const response = await fetch('https://fakestoreapi.com/products');
+            const data = await response.json();
+            return data;
+        }
+        catch (error) {
+            console.error('Error fetching products:', error);
+            return [];
+        }
+}
+}
